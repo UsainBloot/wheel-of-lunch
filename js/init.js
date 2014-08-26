7bf949bd0f26ea31c1e881adc158f511600682d4
Jack Palmer <jackpalmer2@gmail.com>
2014-08-26 22:22:20 +0100

    Removal of getUrlParams in init.js.

--- js/init.js
+++ js/init.js
@@ -45,19 +45,6 @@ function viewShareLink() {
 	window.location.href = createURL();
 }
 
-function getUrlParams() {
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++) {
-        hash = hashes[i].split('=');
-        if(hash.length == 2) {
-	        vars.push(hash[0]);
-	        vars[hash[0]] = hash[1];
-	    }
-    }
-    return vars;
-}
-
 function createURL() {
 	var url = window.location.href + "?";
 	var params = { 'lat': $('#latitude').val(), 
