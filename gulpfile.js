606d72a926f51bf699d649007617eccd94029bd3
Jack Palmer <jackpalmer2@gmail.com>
2015-07-28 23:08:52 +0100

    Implmented browser-sync gulp task

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+// watch files for changes and reload
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
+});
